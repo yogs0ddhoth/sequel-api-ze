77530e59cb709987592581801e66878eb6f3e7ed
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-04-02 13:06:44 -0400

    models set up

--- models/index.js
+++ models/index.js
@@ -4,13 +4,18 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category);
+Category.hasMany(Product);
 
-// Categories have many Products
+Product.belongsToMany(Tag, {through: {
+  model: ProductTag,
+  unique: false
+}});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {through: {
+  model: ProductTag,
+  unique: false
+}});
 
 module.exports = {
   Product,
